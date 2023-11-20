37cd3180ddc7254c8518bdcf9bc4724d0d38406b
Ziwei Su <edwinsu96@gmail.com>
2023-11-20 12:08:01 -0600

    update inference

--- inference_unet.py
+++ inference_unet.py
@@ -4,25 +4,33 @@ marcelogennari@outlook.com
 
 This script performs the sampling given the trained UNet model
 """
-from tqdm import trange
 
+from tqdm import trange
 import torch
 from models import UNet
 from diffusion_model import DiffusionProcess
-
 import matplotlib.pyplot as plt
+%matplotlib inline
 
 if __name__ == "__main__":
     # Prepare model
-    device = "cpu"
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     batch_size = 100
     model = UNet().to(device)
-    model.load_state_dict(torch.load("unet_mnist.pth"))
-    process = DiffusionProcess()
 
+    # Load state dictionary and remove unexpected keys
+    state_dict = torch.load("unet_mnist.pth")
+    unexpected_keys = []
+    for key in state_dict.keys():
+        if key not in model.state_dict():
+            unexpected_keys.append(key)
+    for key in unexpected_keys:
+        del state_dict[key]
+    model.load_state_dict(state_dict)
+    process = DiffusionProcess()
+    
     # Sampling
     xt = torch.randn(batch_size, 1, 28, 28)
-
     model.eval()
     with torch.no_grad():
         for t in trange(999, -1, -1):
@@ -38,3 +46,4 @@ if __name__ == "__main__":
         plt.imshow(xt[i][0], cmap="gray", interpolation="none")
         plt.title(labels[i])
     plt.show()
+
